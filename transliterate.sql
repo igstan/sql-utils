6d6a76192480666e8ff8337de4d4f6ace5fda983
Ionuț G. Stan <ionut.g.stan@gmail.com>
2012-10-30 00:22:36 +0200

    Preserve character case. Contribution by Gabriel Humeniuc
    See this comment:
    
      http://igstan.ro/posts/2009-02-13-mysql-transliteration-function.html#comment-183108903

--- transliterate.sql
+++ transliterate.sql
@@ -8,39 +8,41 @@ BEGIN
   DECLARE len      INT(3)       DEFAULT 0;
   DECLARE pos      INT(3)       DEFAULT 1;
   DECLARE letter   CHAR(1);
+  DECLARE is_lower BIT;
 
   SET len = CHAR_LENGTH(original);
 
   WHILE (pos <= len) DO
-    SET letter = SUBSTRING(original, pos, 1);
+    SET letter   = SUBSTRING(original, pos, 1);
+    SET is_lower = IF(LCASE(letter) COLLATE utf8_bin = letter COLLATE utf8_bin, 1, 0);
 
     CASE TRUE
-      WHEN letter = 'a' THEN SET letter = 'a';
-      WHEN letter = 'b' THEN SET letter = 'b';
-      WHEN letter = 'c' THEN SET letter = 'c';
-      WHEN letter = 'd' THEN SET letter = 'd';
-      WHEN letter = 'e' THEN SET letter = 'e';
-      WHEN letter = 'f' THEN SET letter = 'f';
-      WHEN letter = 'g' THEN SET letter = 'g';
-      WHEN letter = 'h' THEN SET letter = 'h';
-      WHEN letter = 'i' THEN SET letter = 'i';
-      WHEN letter = 'j' THEN SET letter = 'j';
-      WHEN letter = 'k' THEN SET letter = 'k';
-      WHEN letter = 'l' THEN SET letter = 'l';
-      WHEN letter = 'm' THEN SET letter = 'm';
-      WHEN letter = 'n' THEN SET letter = 'n';
-      WHEN letter = 'o' THEN SET letter = 'o';
-      WHEN letter = 'p' THEN SET letter = 'p';
-      WHEN letter = 'q' THEN SET letter = 'q';
-      WHEN letter = 'r' THEN SET letter = 'r';
-      WHEN letter = 's' THEN SET letter = 's';
-      WHEN letter = 't' THEN SET letter = 't';
-      WHEN letter = 'u' THEN SET letter = 'u';
-      WHEN letter = 'v' THEN SET letter = 'v';
-      WHEN letter = 'w' THEN SET letter = 'w';
-      WHEN letter = 'x' THEN SET letter = 'x';
-      WHEN letter = 'y' THEN SET letter = 'y';
-      WHEN letter = 'z' THEN SET letter = 'z';
+      WHEN letter = 'a' THEN SET letter = IF(is_lower, 'a', 'A');
+      WHEN letter = 'b' THEN SET letter = IF(is_lower, 'b', 'B');
+      WHEN letter = 'c' THEN SET letter = IF(is_lower, 'c', 'C');
+      WHEN letter = 'd' THEN SET letter = IF(is_lower, 'd', 'D');
+      WHEN letter = 'e' THEN SET letter = IF(is_lower, 'e', 'E');
+      WHEN letter = 'f' THEN SET letter = IF(is_lower, 'f', 'F');
+      WHEN letter = 'g' THEN SET letter = IF(is_lower, 'g', 'G');
+      WHEN letter = 'h' THEN SET letter = IF(is_lower, 'h', 'H');
+      WHEN letter = 'i' THEN SET letter = IF(is_lower, 'i', 'I');
+      WHEN letter = 'j' THEN SET letter = IF(is_lower, 'j', 'J');
+      WHEN letter = 'k' THEN SET letter = IF(is_lower, 'k', 'K');
+      WHEN letter = 'l' THEN SET letter = IF(is_lower, 'l', 'L');
+      WHEN letter = 'm' THEN SET letter = IF(is_lower, 'm', 'M');
+      WHEN letter = 'n' THEN SET letter = IF(is_lower, 'n', 'N');
+      WHEN letter = 'o' THEN SET letter = IF(is_lower, 'o', 'O');
+      WHEN letter = 'p' THEN SET letter = IF(is_lower, 'p', 'P');
+      WHEN letter = 'q' THEN SET letter = IF(is_lower, 'q', 'Q');
+      WHEN letter = 'r' THEN SET letter = IF(is_lower, 'r', 'R');
+      WHEN letter = 's' THEN SET letter = IF(is_lower, 's', 'S');
+      WHEN letter = 't' THEN SET letter = IF(is_lower, 't', 'T');
+      WHEN letter = 'u' THEN SET letter = IF(is_lower, 'u', 'U');
+      WHEN letter = 'v' THEN SET letter = IF(is_lower, 'v', 'V');
+      WHEN letter = 'w' THEN SET letter = IF(is_lower, 'w', 'W');
+      WHEN letter = 'x' THEN SET letter = IF(is_lower, 'x', 'X');
+      WHEN letter = 'y' THEN SET letter = IF(is_lower, 'y', 'Y');
+      WHEN letter = 'z' THEN SET letter = IF(is_lower, 'z', 'Z');
       ELSE
         SET letter = letter;
     END CASE;
